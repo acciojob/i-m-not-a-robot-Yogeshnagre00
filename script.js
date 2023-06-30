e626ed61fd0483e36b70a188b6bfe0f910e6a86c
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-30 14:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
-//your JS code here. If required.
-const images = [
-  "url-to-image-1",
-  "url-to-image-2",
-  "url-to-image-3",
-  "url-to-image-4",
-  "url-to-image-5"
-];
+//your code here
+let resetbtn = document.querySelector("#reset");
+let verifybtn = document.querySelector("#verify");
 
-// Choose a random image to repeat
-const repeatImage = images[Math.floor(Math.random() * images.length)];
 
-// Add a duplicate image to the array
-images.push(repeatImage);
+let imgtag = document.createElement("img");
+let randomvalue = Math.floor(Math.random()*5) +1;
+imgtag.setAttribute("class",`img${randomvalue}`);
+document.querySelector(".flex").appendChild(imgtag);
 
-// Shuffle the array
-for (let i = images.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [images[i], images[j]] = [images[j], images[i]];
-}
 
-// Assign class names to each image tag
-const imgTags = document.querySelectorAll("img");
-for (let i = 0; i < imgTags.length; i++) {
-  imgTags[i].classList.add("img" + (i+1));
-  imgTags[i].src = images[i];
-}
+let images = document.querySelectorAll("img");
 
-let selectedImages = [];
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        image.classList.toggle("selected");
 
-// Add click event listeners to images
-imgTags.forEach(img => {
-  img.addEventListener("click", () => {
-    if (selectedImages.length < 2) {
-      img.classList.add("selected");
-      selectedImages.push(img);
-    }
-    if (selectedImages.length === 2) {
-      const verifyButton = document.getElementById("verify");
-      verifyButton.classList.remove("hidden");
-      verifyButton.addEventListener("click", () => {
-        if (selectedImages[0].classList[0] === selectedImages[1].classList[0]) {
-          document.getElementById("para").textContent = "You are a human. Congratulations!";
-        } else {
-          document.getElementById("para").textContent = "We can't verify you as a human. You selected the non-identical tiles.";
+        if(isrestbtnvalid()){
+         resetbtn.style.display="inline-block";
         }
-        verifyButton.classList.add("hidden");
-      });
-    }
-    const resetButton = document.getElementById("reset");
-    resetButton.classList.remove("hidden");
-    resetButton.addEventListener("click", () => {
-      selectedImages.forEach(img => {
-        img.classList.remove("selected");
-      });
-      selectedImages = [];
-      document.getElementById("verify").classList.add("hidden");
-      resetButton.classList.add("hidden");
-      document.getElementById("para").textContent = "";
+        else{
+            resetbtn.style.display="none";
+        }
+
+        if(isverifybtnvalid()){
+            verifybtn.style.display="inline-block";
+           }
+           else{
+               verifybtn.style.display="none";
+           }
+
     });
-  });
+
+});
+
+
+function isrestbtnvalid(){
+    for(let i=0;i<images.length;i++){
+        if(images[i].classList.contains("selected")){
+            return true;
+        }      
+    }
+    return false;
+}
+
+function isverifybtnvalid(){
+    let count=0;
+    for(let i=0;i<images.length;i++){
+        if(images[i].classList.contains("selected")){
+            count++;
+        }      
+    }
+    return (count==2);
+}
+
+resetbtn.addEventListener("click", () => {
+    for(let i=0;i<images.length;i++){
+        if(images[i].classList.contains("selected")){
+            images[i].classList.remove("selected");
+        }      
+    }
+    resetbtn.style.display="none";
+
+    let ps = document.querySelectorAll("p");
+    ps.forEach((p) =>{
+        document.querySelector("main").removeChild(p);
+    });
+    
+});
+
+verifybtn.addEventListener("click", () => {
+    let p = document.createElement("p");
+    p.setAttribute("id","para");
+
+
+  let classarr = document.querySelectorAll(".selected");
+  if(classarr[0].getAttribute("class")==classarr[1].getAttribute("class")){
+     p.innerText = "You are a human. Congratulations!"
+  }
+  else{
+    p.innerText = "We can't verify you as a human. You selected the non-identical tiles."
+  }
+
+  document.querySelector("main").appendChild(p);
+  verifybtn.style.display="none";
+
+  for(let i=0;i<images.length;i++){
+    if(images[i].classList.contains("selected")){
+        images[i].classList.remove("selected");
+    }      
+ }
+   
 });
